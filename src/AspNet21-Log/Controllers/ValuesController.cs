a7b83c8d46721f6775b70e1068f682cf9e02c365
GUILHERME BALDINI <guilhermebaldini@hotmail.com>
2018-08-07 22:04:31 -0300

    Initial Project

---
+++ src/AspNet21-Log/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace AspNet21_Log.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var value = "Return value";
+
+            _logger.LogInformation("O retorno foi: {value}", value);
+
+            return value;
+        }
+    }
+}
---
+++ src/AspNet21-Log/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using System;
+
+namespace AspNet21_Log
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseSerilog((hostingContext, cfg) =>
+            {
+                cfg.WriteTo.Elasticsearch(new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri("http://elk:9200")));
+            })
+            .UseStartup<Startup>();
+    }
+}
